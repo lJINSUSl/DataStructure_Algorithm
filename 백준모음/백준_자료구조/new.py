9bee362b93ed2593707d92f81531f735f16bb25b
lJINSUSl <85980131+lJINSUSl@users.noreply.github.com>
2023-09-05 11:01:22 +0900

    230905

--- 백준모음/백준_자료구조/new.py
+++ 백준모음/백준_자료구조/new.py
@@ -35,16 +35,41 @@ from itertools import combinations
 #
 # print(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"],["green_turban", "headgr"],["green_turban", "headgr"],["green_turban", "headgr"]]))
 
-def solution(genres, plays):
-    dictt = []
-    for i in range(len(genres)):
-        dictt.append((genres[i],plays[i]))
-    print(dictt)
-    dic = collections.Counter(dictt)
-    num = dic.most_common()
-    print(num)
+# def solution(genres, plays):
+#     dictt = []
+#     for i in range(len(genres)):
+#         dictt.append((genres[i],plays[i]))
+#     print(dictt)
+#     dic = collections.Counter(dictt)
+#     num = dic.most_common()
+#     print(num)
+#
+#     answer = []
+#     return answer
+#
+# solution(["classic", "pop", "classic", "classic", "pop"],[500, 600, 150, 800, 2500])
 
+def solution(progresses, speeds):
     answer = []
+    plus = []
+    num = 0
+    nu = len(progresses)
+    while sum(answer) < nu:
+        for i in range(len(progresses)):
+            progresses[i] = progresses[i] + speeds[i]
+
+        print(progresses)
+        while progresses[0] >= 100:
+            num += 1
+            progresses.pop(0)
+            speeds.pop(0)
+        print(progresses)
+
+        if num > 0:
+            answer.append(num)
+            num = 0
+        print(answer, sum(answer), len(speeds))
+
     return answer
 
-solution(["classic", "pop", "classic", "classic", "pop"],[500, 600, 150, 800, 2500])
+print(solution([93,30,35],[1,30,5]))
